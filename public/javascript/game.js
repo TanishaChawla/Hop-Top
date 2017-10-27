2657f30fa55084e3dd06a932ddaa1b579aae10b9
TanishaChawla <chawlatanisha20@gmail.com>
2017-10-27 09:45:44 +0530

    minor

--- public/javascript/game.js
+++ public/javascript/game.js
@@ -1,11 +1,12 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 function preload(){
   game.load.image('cloud','images/cloud-hi.png');
-  game.load.spritesheet('balloon','images/balloon.png',40,40);
+  game.load.image('balloon','images/balloon.png',40,40);
 
 }
 function create(){
-  game.add.sprite(10,10,'cloud');
+  game.add.sprite(0,0,'cloud');
+  game.add.sprite(5,5, 'balloon');
 }
 function update(){
 
